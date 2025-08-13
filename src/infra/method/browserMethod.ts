eeadd84c98a7beecbd5d965c8c49a50eda1a59e9
Flavio Coscarella <flaviodc@live.com.ar>
2025-08-13 16:35:25 -0300

    Update: browserMethod to match the currect use for the services

--- src/infra/method/browserMethod.ts
+++ src/infra/method/browserMethod.ts
@@ -1,17 +1,25 @@
 import { chromium } from '@playwright/test';
+import { scrapDynatraceBrowser } from '@services/dynatrace';
+import { scrapGmailBrowser } from '@services/gmail';
 
 export const scrapFromBrowser = async () => {
-  const browser = await chromium.launch({ headless: false });
+  const win_user = process.env.WIN_USER ?? '';
 
-  const page = await browser.newPage();
+  // Define the path to the Chrome user data directory
+  const path = `C:/Users/${win_user}/AppData/Local/Google/Chrome/User Data`;
 
-  const url = process.env.MAIN_SITE ?? '';
+  const browser = await chromium.launchPersistentContext(path, {
+    headless: false,
+    args: [
+      '--disable-blink-features=AutomationControlled',
+      '--no-sandbox',
+      '--disable-infobars',
+    ],
+  });
+  
+  await scrapDynatraceBrowser(browser)
 
-  if (!url) {
-    throw new Error('MAIN_SITE environment variable is not defined');
-  }
+  await scrapGmailBrowser(browser);
 
-  await page.goto(url);
-
-  await browser.close();
+  // await browser.close();
 };
