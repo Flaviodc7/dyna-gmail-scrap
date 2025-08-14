1dffec82d882d2540c15aeae5592e80b62e71a74
Flavio Coscarella <flaviodc@live.com.ar>
2025-08-14 16:35:25 -0300

    Refactoring: Moving env detection into utils

---
+++ src/infra/utils/dynatrace/checkDynatraceEnvs.ts
@@ -0,0 +1,18 @@
+export const checkEnvsDynatrace = () => {
+  
+  const dynatrace_url = process.env.DYNATRACE_URL ?? '';
+
+  const dynatrace_user = process.env.DYNATRACE_USER ?? '';
+
+  const dynatrace_pass = process.env.DYNATRACE_PASS ?? '';
+
+  if (!dynatrace_url || !dynatrace_user || !dynatrace_pass) {
+    throw new Error('DYNATRACE_URL or DYNATRACE_USER or DYNATRACE_PASS environment variables are not defined');
+  }
+
+  return {
+    dynatrace_url,
+    dynatrace_user,
+    dynatrace_pass
+  };
+};
