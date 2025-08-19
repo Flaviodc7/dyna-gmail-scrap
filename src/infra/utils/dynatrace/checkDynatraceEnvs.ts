9b771955582005c5d1ddcd906a591190160372c5
Flavio Coscarella <flaviodc@live.com.ar>
2025-08-19 18:34:01 -0300

    Refactor: adding new env vars

--- src/infra/utils/dynatrace/checkDynatraceEnvs.ts
+++ src/infra/utils/dynatrace/checkDynatraceEnvs.ts
@@ -1,17 +1,20 @@
 export const checkEnvsDynatrace = () => {
-  const dynatrace_url = process.env.DYNATRACE_URL ?? '';
-
   const dynatrace_user = process.env.DYNATRACE_USER ?? '';
 
   const dynatrace_pass = process.env.DYNATRACE_PASS ?? '';
 
+  const dynatrace_main_url = process.env.DYNATRACE_MAIN_URL ?? '';
+
   const dynatrace_dashboard_url = process.env.DYNATRACE_DASHBOARD_URL ?? '';
 
+  const dynatrace_problems_url = process.env.DYNATRACE_PROBLEMS_URL ?? '';
+
   if (
-    !dynatrace_url ||
     !dynatrace_user ||
     !dynatrace_pass ||
-    !dynatrace_dashboard_url
+    !dynatrace_main_url ||
+    !dynatrace_dashboard_url ||
+    !dynatrace_problems_url
   ) {
     throw new Error(
       'DYNATRACE_URL or DYNATRACE_USER or DYNATRACE_PASS or DYNATRACE_DASHBOARD_URL environment variables are not defined',
@@ -19,9 +22,10 @@ export const checkEnvsDynatrace = () => {
   }
 
   return {
-    dynatrace_url,
     dynatrace_user,
     dynatrace_pass,
+    dynatrace_main_url,
     dynatrace_dashboard_url,
+    dynatrace_problems_url,
   };
 };
