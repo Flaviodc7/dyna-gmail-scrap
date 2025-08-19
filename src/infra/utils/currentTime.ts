ab86d67e9ba72aa8f324f96a7ce81132d17d969c
Flavio Coscarella <flaviodc@live.com.ar>
2025-08-19 18:34:57 -0300

    Refactor: returning detailed parts for every part of the date

--- src/infra/utils/currentTime.ts
+++ src/infra/utils/currentTime.ts
@@ -9,5 +9,21 @@ export const currentTime = () => {
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
 
-  return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+  const dateDay = `${year}-${month}-${day}`;
+
+  const timecheckHoliday = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} GMT-0300`;
+
+  const timeScreenshot = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+
+  return {
+    year,
+    month,
+    day,
+    hours,
+    minutes,
+    seconds,
+    dateDay,
+    timeScreenshot,
+    timecheckHoliday,
+  };
 };
